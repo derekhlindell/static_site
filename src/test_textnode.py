c540929bbbf0f41372a07ef6195e943d5ccf1a70
derek <my_email@email.com>
2024-06-29 15:45:47 -0700

    first publish

---
+++ src/test_textnode.py
@@ -0,0 +1,39 @@
+import unittest
+
+from textnode import TextNode, text_type_text
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a text node", "bold")
+        self.assertEqual(node, node2)
+    
+    def test_eq_url(self):
+        node = TextNode("This is a text node", "bold", "Boot.dev")
+        node2 = TextNode("This is a text node", "bold", "Boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_different_url(self):
+        node = TextNode("This is a text node", "bold", None)
+        node2 = TextNode("This is a text node", "bold", "Boot.dev")
+        self.assertNotEqual(node, node2)
+    
+    def test_different_text(self):
+        node = TextNode("This is a text node", "bold",)
+        node2 = TextNode("This is also a text node", "bold",)
+        self.assertNotEqual(node, node2)
+
+    def test_different_style(self):
+        node = TextNode("This is a text node", "bold",)
+        node2 = TextNode("This is a text node", "italics",)
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
+if __name__ == "__main__":
+    unittest.main()
